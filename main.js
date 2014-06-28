f00f5c06af82ce9fda56a1f7b2a6775db0b71411
toran billups <toranb@gmail.com>
2014-06-28 18:59:24 -0500

    init

---
+++ main.js
@@ -0,0 +1,25 @@
+define(["exports"], function(__exports__) {
+    "use strict";
+
+    var PromiseMixin = (function() {
+        var mixin = function() {
+        };
+        mixin.prototype.promise = function(url, type, hash) {
+            return new RSVP.Promise(function(resolve, reject) {
+              hash.success = function(json) {
+                return resolve(json);
+              };
+              hash.error = function(json) {
+                if (json && json.then) {
+                  json.then = null;
+                }
+                return reject(json);
+              };
+              $.ajax(hash);
+            });
+        }
+        return mixin;
+    })();
+
+    __exports__["default"] = PromiseMixin;
+});
